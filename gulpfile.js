d0b9e94081ba066ecc7b3ecc3248fca40b680767
andrewprokopenko <i.andrewprokopenko@gmail.com>
2020-04-12 21:39:03 +0300

    add text about trigger

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,27 @@ var rename = require('gulp-rename');
 
 // gulp.task('default', ['customScript', 'labsScript'])
 
-// gulp.task('default', function() {
-//   return gulp.src('js/*.js')
-//     .pipe(concat('script.js'))
-//     .pipe(jsmin())
-//     .pipe(rename({suffix: '.min'}))
-//     .pipe(gulp.dest('./dist/'));
-// });
-
-
 gulp.task('default', function() {
-  return gulp.src('libs/*.js')
-    .pipe(order([
-        "jquery-3.4.1.slim.min.js",
-        "popper.min.js",
-        "bootstrap.min.js",
-        "selectr.min.js",
-        "swiper.min.js",
-    ]))
-    .pipe(concat('vendor.js'))     
+  return gulp.src('js/*.js')
+    .pipe(concat('script.js'))
     .pipe(jsmin())
-    .pipe(rename({suffix: '.min'}))                            
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/'));
 });
+
+
+// gulp.task('default', function() {
+//   return gulp.src('libs/*.js')
+//     .pipe(order([
+//         "jquery-3.4.1.slim.min.js",
+//         "popper.min.js",
+//         "bootstrap.min.js",
+//         "selectr.min.js",
+//         "swiper.min.js",
+//     ]))
+//     .pipe(concat('vendor.js'))     
+//     .pipe(jsmin())
+//     .pipe(rename({suffix: '.min'}))                            
+//     .pipe(gulp.dest('./dist/'));
+// });
 
--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,20 @@
  
   
 let modalBtn = document.querySelectorAll('.js-modal-btn a'),
-modalCont = document.querySelector('.js-modal'),
-modalCloseCont = document.querySelector('.js-modal .service-modal-container'),
-modalCloseIcon = document.querySelectorAll('.modal-close'),
-closeBtn = document.querySelector('.js-close-bnt'),
-modalContact = document.querySelector('.service-modal-container_contact'),
-modalEnd = document.querySelector('.service-modal-container_end') 
+    modalCont = document.querySelector('.js-modal'),
+    modalCloseCont = document.querySelector('.js-modal .service-modal-container'),
+    modalCloseIcon = document.querySelectorAll('.modal-close'),
+    closeBtn = document.querySelector('.js-close-bnt'),
+    modalContact = document.querySelector('.service-modal-container_contact'),
+    modalEnd = document.querySelector('.service-modal-container_end') 
 
  
-for (let i = 0 ; i< modalBtn.length; i++) { 
-modalBtn[i].addEventListener('click', openModal)
+for (let i = 0 ; i < modalBtn.length; i++) { 
+    modalBtn[i].addEventListener('click', openModal)
 }
  
-for (let i = 0 ; i< modalCloseIcon.length; i++) { 
-modalCloseIcon[i].addEventListener('click', closeModal)
+for (let i = 0 ; i < modalCloseIcon.length; i++) { 
+    modalCloseIcon[i].addEventListener('click', closeModal)
 }
 
 modalCont.addEventListener('click', closeModalProps) 
@@ -23,33 +23,38 @@ closeBtn.addEventListener('click', closeBtnFunction)
  
 
 function openModal(e) { 
-document.body.classList.add('overflow-hidden')
-e.preventDefault()
-modalCont.classList.add('show') 
+    console.log('openModal')
+    document.body.classList.add('overflow-hidden')
+    e.preventDefault()
+    modalCont.classList.add('show') 
 }
 
 function closeModal() { 
-document.body.classList.remove('overflow-hidden')
-modalCont.classList.remove('show') 
-modalEnd.classList.remove('show')
-modalContact.classList.remove('hide') 
+    console.log('closeModal')
+    document.body.classList.remove('overflow-hidden')
+    modalCont.classList.remove('show') 
+    modalEnd.classList.remove('show')
+    modalContact.classList.remove('hide') 
 }
 function closeModalProps(e) { 
-if( e.target == modalCont ||  e.target == modalCloseCont) {
-document.body.classList.remove('overflow-hidden')
-modalCont.classList.remove('show')
-modalEnd.classList.remove('show')
-modalContact.classList.remove('hide') 
-}  
+    console.log('closeModalProps')
+    if( e.target == modalCont ||  e.target == modalCloseCont) {
+        console.log('closeModalProps if')
+        document.body.classList.remove('overflow-hidden');
+        modalCont.classList.remove('show');
+        modalEnd.classList.remove('show');
+        modalContact.classList.remove('hide');
+    }  
 }
 
-function submitModal() { 
-console.log(1)
-modalContact.classList.add('hide')
-modalEnd.classList.add('show')
+function submitModal() {  
+    console.log('submit') 
+    modalEnd.classList.add('show')
+    modalContact.classList.add('hide')
 }
 
 function closeBtnFunction() { 
+    console.log('closeBtnFunction')
 closeModal()
 modalEnd.classList.remove('show')
 modalContact.classList.remove('hide')

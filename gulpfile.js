014793bdf60a54156d63d93b11f17fbedf830414
andrewprokopenko <i.andrewprokopenko@gmail.com>
2020-04-13 12:51:18 +0300

    table service add

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var rename = require('gulp-rename');
 
 // gulp.task('default', ['customScript', 'labsScript'])
 
-gulp.task('default', function() {
+gulp.task('script', function() {
   return gulp.src('js/*.js')
     .pipe(concat('script.js'))
     .pipe(jsmin())
@@ -15,18 +15,18 @@ gulp.task('default', function() {
 });
 
 
-// gulp.task('default', function() {
-//   return gulp.src('libs/*.js')
-//     .pipe(order([
-//         "jquery-3.4.1.slim.min.js",
-//         "popper.min.js",
-//         "bootstrap.min.js",
-//         "selectr.min.js",
-//         "swiper.min.js",
-//     ]))
-//     .pipe(concat('vendor.js'))     
-//     .pipe(jsmin())
-//     .pipe(rename({suffix: '.min'}))                            
-//     .pipe(gulp.dest('./dist/'));
-// });
+gulp.task('vendor', function() {
+  return gulp.src('libs/*.js')
+    .pipe(order([
+        "jquery-3.4.1.slim.min.js",
+        "popper.min.js",
+        "bootstrap.min.js",
+        "selectr.min.js",
+        "swiper.min.js",
+    ]))
+    .pipe(concat('vendor.js'))     
+    .pipe(jsmin())
+    .pipe(rename({suffix: '.min'}))                            
+    .pipe(gulp.dest('./dist/'));
+});
 
